84a37021809c5ad8311d03b6ed42e3597bb452b2
YANA <yana@thefloating.cloud>
2021-01-26 19:57:56 -0500

    (WIP) ideas for Charlies behaviors

--- charlie.cr
+++ charlie.cr
@@ -30,6 +30,17 @@ class Charlie
         match = line.match(/^:(.+)!.+ PRIVMSG #\w+ :(.+)$/)
 
         # do some stuff in response to messages
+        # commands (hash)
+        # => !sauce, !uptime, !github ..
+        # matching other stuf in the messages... (case)
+
+        # make another program, "What Would ____ Do?"
+        # which is just bunch of commands => response
+        # and it compiles this to a file.json
+        # that Charlie can reference?
+
+        # Charlie will eventually get a database (sqlite db that will be just files! :>)
+
         if match
           user = match[1]
           message = match[2]

d86a49cd64edb4c4d6bdd24633b54487c7381207
YANA <30638175+WhereIsX@users.noreply.github.com>
2021-01-29 19:15:01 -0500

    made cmd_help

--- command.cr
+++ command.cr
@@ -1,5 +1,26 @@
-# key has to fit exactly
-#   !uptime, !lurk
+COMMAND_VOCABULARY = {
+  "!help"     => ->cmd_help(String),
+  "!commands" => ->cmd_help(String),
+  "!echo"     => ->cmd_echo(String),
+  "!emojis"   => ->cmd_emojis(String),
+  "!ping"     => ->cmd_ping(String),
+}
+
+PONG_FACTS = [
+  ":ping_pong: is a nice game",
+  ":ping_pong: yourself",
+  ":ping_pong: https://en.wikipedia.org/wiki/Pong",
+  ":ping_pong: gers",
+  "29 November 1972 is when :ping_pong: was released",
+  ":ping_pong: was the first game developed by atari",
+  ":ping_pong: https://en.wikipedia.org/wiki/Pong#/media/File:Signed_Pong_Cabinet.jpg",
+]
+
+def cmd_help(stuff : String)
+  intro_bit = "the commands are: "
+  keys_bit = COMMAND_VOCABULARY.keys.sort.join(" | ")
+  return intro_bit + keys_bit
+end
 
 def cmd_echo(stuff : String)
   if stuff.empty?
@@ -11,23 +32,13 @@ def cmd_echo(stuff : String)
   end
 end
 
-COMMAND_VOCABULARY = {
-  # "!help" =>
-  # "!commands" =>
-  "!echo"   => ->cmd_echo(String),
-  "!emojis" => ->(stuff : String) { return "Waaat Shears Woool QuackTogether" },
-  "!ping"   => ->(stuff : String) { return PONG_FACTS.sample },
-}
+def cmd_emojis(stuff : String)
+  return "Waaat Shears Woool QuackTogether"
+end
 
-PONG_FACTS = [
-  ":ping_pong: is a nice game",
-  ":ping_pong: yourself",
-  ":ping_pong:: https://en.wikipedia.org/wiki/Pong",
-  ":ping_pong:gers",
-  "29 November 1972 is when :ping_pong: was released",
-  ":ping_pong: was the first game developed by atari",
-  ":ping_pong: https://en.wikipedia.org/wiki/Pong#/media/File:Signed_Pong_Cabinet.jpg",
-]
+def cmd_ping(stuff : String)
+  return PONG_FACTS.sample
+end
 
 TWITCH_MOD_COMMANDS = [
   "/ban",

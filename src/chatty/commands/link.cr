11d6491ea5669ecfa6692377d613399765db335c
YANA <30638175+WhereIsX@users.noreply.github.com>
2021-05-10 16:02:33 -0400

    ?

---
+++ src/chatty/commands/link.cr
@@ -0,0 +1,17 @@
+require "./command.cr"
+
+Command.new(
+  name: "!link",
+  description: "!link <optional: address>",
+  species: "dynamic"
+) do |ircm|
+  case ircm.words.size
+  when 1
+    next Command.link
+  when 2
+    Command.link = words[1]
+    next "got ya baws :>"
+  else
+    next "ya wat. it's !link <optional: address>"
+  end
+end
